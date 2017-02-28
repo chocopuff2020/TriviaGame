19f287508059b6f07ff23d22c06105be104a6e90
Michelle Dai <michelledai2020@gmail.com>
2017-02-28 14:20:09 -0800

    Need fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,12 +46,91 @@ var answerAray = [
 var questionContainer = document.querySelector('.question-container');
 var answerOption = document.querySelector('.answer-options');
 
+/*=============================
+=            TIMER            =
+=============================*/
+// var intervalId;
+// var stopwatch = {
+//   time: 0,
+
+//   reset: function() {
+//     stopwatch.time = 0;
+//     $("#display").html("00:00");
+//   },
+
+//   start: function() {
+//     intervalId = setInterval(stopwatch.count, 1000*30);
+//   },
+
+//   stop: function() {
+//     clearInterval(intervalId);
+//   },
+
+//   count: function() {
+//     stopwatch.time++;
+//     var converted = stopwatch.timeConverter(stopwatch.time);
+//     console.log(converted);
+//     $("#display").html(converted);
+//   },
+
+//   timeConverter: function(t) {
+//     var minutes = Math.floor(t / 60);
+//     var seconds = t - (minutes * 60);
+//     if (seconds < 10) {
+//       seconds = "0" + seconds;
+//     }
+//     if (minutes === 0) {
+//       minutes = "00";
+//     }
+//     else if (minutes < 10) {
+//       minutes = "0" + minutes;
+//     }
+//     return minutes + ":" + seconds;
+//   }
+// };
+
+
+var correctOption;
+
+
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
+//             Game.correctAnswer();
+
+//             alert("The correct answer is "+ correctOption);
+//         }
+//     }, 1000);
+// }
+
+// window.onload = function () {
+//     var countdownFrom = 30,
+//         display = document.querySelector('#display');
+//     startTimer(countdownFrom, display);
+// };
+
+/*=====  End of TIMER  ======*/
+
+
+
+
 /*============================
 =            Game            =
 ============================*/
 var currentQuestion;
 var currentAnswerPack;
 var randomNumber;
+
 
 function Game() {
   this.question = null;
@@ -65,6 +144,8 @@ Game.prototype.start = function () {
     randomNumber = Math.floor(Math.random()*questionArray.length);
     this.flush();
     this.evaluate();
+    this.correctAnswer();
+
 }
 
 
@@ -72,6 +153,15 @@ Game.prototype.evaluate = function() {
     $('.options').click(function() {
       alert($(this).attr('data-val'));
     })
+}
+
+Game.prototype.correctAnswer = function() {
+    for(key in currentAnswerPack) {
+      if (currentAnswerPack[key] == true) {
+        correctOption = key;
+        console.log(correctOption);
+      }
+    }
 }
 
 
@@ -85,6 +175,32 @@ Game.prototype.flush = function() {
      }
 }
 
+Game.prototype.startTimer = function(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+                this.correctAnswer();
+                alert("The correct answer is "+ correctOption);
+            }
+        }, 1000);
+}
+
 var game = new Game();
 game.start();
+
+window.onload = function () {
+            var countdownFrom = 30,
+            display = document.querySelector('#display');
+            Game.startTimer(countdownFrom, display);
+};
+
 
